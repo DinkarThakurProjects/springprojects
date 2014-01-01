86dc067347b48d5ae695bc2ffbb78f74729f7cdd
dinkarthakur <thakur.dinkar@gmail.com>
2014-01-01 13:13:54 +0530

    First Commit

---
+++ src/com/tutorialspoint/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.tutorialspoint;
+
+public class HelloWorld {
+	private String message;
+
+	/**
+	 * @return the message
+	 */
+	public void getMessage() {
+		System.out.println("Your Message : " + message);
+	}
+
+	/**
+	 * @param message
+	 *            the message to set
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
